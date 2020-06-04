149d2517bf4a552e00311e431fdeef9633ac07d2
Anh Vuong <vuonga1103@yahoo.com>
2020-06-04 19:53:11 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -128,28 +128,31 @@ def game_hash
   }
 end
 
+# Helper function that takes in a team type (i.e. home or away) and returns all of the stats of the players who are in that team
+def all_players(team_type)
+  game_hash[team_type][:players]
+end
+
 
 def num_points_scored(player_name)
 
-  game_hash[:home][:players].each do |player_hash|
-    return player_hash[:points] if player_hash[:player_name] == player_name
-  end
-
-  game_hash[:away][:players].each do |player_hash|
-    return player_hash[:points] if player_hash[:player_name] == player_name
+  game_hash.each do |team_type, team_hash|
+    all_players(team_type).each do |player_hash|
+      return player_hash[:points] if player_hash[:player_name] == player_name
+    end
   end
 
 end
 
 
 def shoe_size(player_name)
-  game_hash[:home][:players].each do |player_hash|
-    return player_hash[:shoe] if player_hash[:player_name] == player_name
-  end
 
-  game_hash[:away][:players].each do |player_hash|
-    return player_hash[:shoe] if player_hash[:player_name] == player_name
+  game_hash.each do |team_type, team_hash|
+    all_players(team_type).each do |player_hash|
+      return player_hash[:shoe] if player_hash[:player_name] == player_name
+    end
   end
+    
 end
 
 
@@ -211,5 +214,5 @@ def big_shoe_rebounds
       return player_hash[:rebounds] if player_hash[:shoe] == biggest_shoe_size
     end
   end
-  
+
 end
