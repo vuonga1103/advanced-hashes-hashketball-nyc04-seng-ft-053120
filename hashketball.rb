1edecd4fb747207e9a1d1438d1941e57fd58723c
Anh Vuong <vuonga1103@yahoo.com>
2020-06-04 19:43:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -135,6 +135,7 @@ def num_points_scored(player_name)
   end
 
   game_hash.each do |team_type, team_hash|
+    binding.pry
     all_players(team_type)[:points] if all_players(team_type)[:player_name] == player_name
   end
   #
